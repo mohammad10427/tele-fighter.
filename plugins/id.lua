d4fb9a758e9ee2ed4011f9c335474648620fe2ba
mohammad10427 <mohammad10427@gmail.com>
2016-07-03 15:35:11 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -1,27 +1,10 @@
-do function run(msg, matches)
-if matches[1]:lower() == 'id' and is_sudo(msg) then
-return "[Sudo]️\n#Your ID : "..msg.from.id.."\n#Your name : "..msg.from.print_name.."\n#Group name : "..msg.to.title.."\n#Group ID : "..msg.to.id.."\n#msg ID : "..msg.id.."\n#Your msg : "..msg.text.."\n#Username : @"..msg.from.username
-end
-if matches[1]:lower() == 'id' and is_admin(msg) then
-return "#Admin️\n#Your ID : "..msg.from.id.."\n#Your name : "..msg.from.print_name.."\n#Group name : "..msg.to.title.."\n#Group ID : "..msg.to.id.."\n#msg ID : "..msg.id.."\n#Your msg : "..msg.text.."\n#Username : @"..msg.from.username
-end
-if matches[1]:lower() == 'id' and is_owner(msg) then
-return "#Owner️\n#Your ID : "..msg.from.id.."\n#Your name : "..msg.from.print_name.."\n#Group name : "..msg.to.title.."\n#Group ID : "..msg.to.id.."\n#msg ID : "..msg.id.."\n#Your msg : "..msg.text.."\n#Username : @"..msg.from.username
-end
-if matches[1]:lower() == 'id' and is_momod(msg) then
-return "#Moderator️\n#Your ID : "..msg.from.id.."\n#Your name : "..msg.from.print_name.."\n#Group name : "..msg.to.title.."\n#Group ID : "..msg.to.id.."\n#msg ID : "..msg.id.."\n#Your msg : "..msg.text.."\n#Username : @"..msg.from.username
-end
-if matches[1]:lower() == 'id' then
-return "#Member️\n#Your ID : "..msg.from.id.."\n#Your name : "..msg.from.print_name.."\n#Group name : "..msg.to.title.."\n#Group ID : "..msg.to.id.."\n#msg ID : "..msg.id.."\n#Your msg : "..msg.text.."\n#Username : @"..msg.from.username
-end
+local function run(msg, matches)
+local id = "〰〰〰〰〰〰〰〰〰\nآیدی شما : "..msg.from.id.."\n〰〰〰〰〰〰〰〰〰\nآیدی گروه : "..msg.to.id.."\n〰〰〰〰〰〰〰〰〰〰BoT Version : 1.0\nId bot : @telesilver"
+return id
 end
 return {
-description = "show your id",
-usage = "!id : show your userid and information",
 patterns = {
-"^[!/#]([Ii][Dd])$",
-"^[Ii][Dd]$",
-},
+"^[!#/][Ii][Dd]$"
+}, 
 run = run
 }
-end
